1366eeb288cd9d3014ff4c95cbbe9db1e97afa3b
weeway <geverway@gmail.com>
2017-11-27 20:14:16 +0800

    the example used to predict also need scaling if feature has been nomarlized

--- machine-learning-ex1/ex1/ex1_multi.m
+++ machine-learning-ex1/ex1/ex1_multi.m
@@ -83,7 +83,7 @@ fprintf('Running gradient descent ...\n');
 
 % Choose some alpha value
 alpha = 0.01;
-num_iters = 400;
+num_iters = 4000;
 
 % Init Theta and Run Gradient Descent 
 theta = zeros(3, 1);
@@ -91,10 +91,37 @@ theta = zeros(3, 1);
 
 % Plot the convergence graph
 figure;
+plot(1:numel(J_history), J_history, '-r', 'LineWidth', 2);
+xlabel('Number of iterations');
+ylabel('Cost J');
+% Display gradient descent's result
+fprintf('Theta computed from gradient descent: \n');
+fprintf(' %f \n', theta);
+fprintf('\n');
+
+% Init Theta and Run Gradient Descent 
+theta = zeros(3, 1);
+[theta, J_history] = gradientDescentMulti(X, y, theta, 0.1, num_iters);
+
+% Plot the convergence graph
+hold on
+plot(1:numel(J_history), J_history, '-g', 'LineWidth', 2);
+xlabel('Number of iterations');
+ylabel('Cost J');
+% Display gradient descent's result
+fprintf('Theta computed from gradient descent: \n');
+fprintf(' %f \n', theta);
+fprintf('\n');
+
+
+% Init Theta and Run Gradient Descent 
+theta = zeros(3, 1);
+[theta, J_history] = gradientDescentMulti(X, y, theta, 1, num_iters);
+% Plot the convergence graph
+hold on
 plot(1:numel(J_history), J_history, '-b', 'LineWidth', 2);
 xlabel('Number of iterations');
 ylabel('Cost J');
-
 % Display gradient descent's result
 fprintf('Theta computed from gradient descent: \n');
 fprintf(' %f \n', theta);
@@ -105,7 +132,7 @@ fprintf('\n');
 % Recall that the first column of X is all-ones. Thus, it does
 % not need to be normalized.
 price = 0; % You should change this
-
+price = [1 ([1650,3]-mu)./sigma]*theta;
 
 % ============================================================
 
@@ -150,7 +177,7 @@ fprintf('\n');
 % Estimate the price of a 1650 sq-ft, 3 br house
 % ====================== YOUR CODE HERE ======================
 price = 0; % You should change this
-
+price = [1,1650,3]*theta;
 
 % ============================================================
 
