0f237895fbfe60384cd938eadd0fc9ff2a114fbd
weeway <geverway@gmail.com>
2017-11-27 19:01:06 +0800

    ex1 completed

--- machine-learning-ex1/ex1/featureNormalize.m
+++ machine-learning-ex1/ex1/featureNormalize.m
@@ -1,5 +1,5 @@
 function [X_norm, mu, sigma] = featureNormalize(X)
-%FEATURENORMALIZE Normalizes the features in X 
+%   FEATURENORMALIZE Normalizes the features in X 
 %   FEATURENORMALIZE(X) returns a normalized version of X where
 %   the mean value of each feature is 0 and the standard deviation
 %   is 1. This is often a good preprocessing step to do when
@@ -26,13 +26,10 @@ sigma = zeros(1, size(X, 2));
 % Hint: You might find the 'mean' and 'std' functions useful.
 %       
 
+mu = mean(X);
+sigma = std(X);
 
-
-
-
-
-
-
+X_norm = (X-ones(1,size(X,1))'*mu)./(ones(1,size(X,1))'*sigma);
 
 % ============================================================
 
